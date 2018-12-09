6610d04fc0ec2a56fdd949dd4173c9af83931216
Zinc Glaze <zekegraves@gmail.com>
2018-12-09 15:34:09 -0500

    fixed form submit and reset behavior. tested get request. fixed data formatting

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,28 @@
 
 var friendsArray = [
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
+    name:"Ahmed",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        3,
+        1,
+        2,
         5,
+        4,
+        1,
         1,
         4,
+        4,
+        5
+      ]
+  },
+  {
+    name:"Marty",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        1,
+        3,
         4,
         5,
         1,
@@ -18,21 +34,22 @@ var friendsArray = [
       ]
   },
   {
-    "name":"Ahmed",
-    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores":[
-        5,
+    name:"Calliope",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
         1,
-        4,
+        3,
         4,
         5,
         1,
         2,
         5,
+        1,
         4,
-        1
+        3
       ]
-  }
+  },
+  
 ];
 
 module.exports = friendsArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,15 +5,16 @@ module.exports = function(app) {
   //API Routes
   //GET requests
   app.get("/api/friends", function(req, res) {
+    //responds with JSON of all friends
     res.json(allFriends);
   });
 
   //POST requests
   app.post("/api/friends", function(req, res) {
+    //responds by pushing request(form data from client) to allFriends array (server data)
     allFriends.push(req.body);
   });
 };
 
-//add logic for compatibility
 
 
