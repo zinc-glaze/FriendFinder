2308f8926659a066e790690839704754cdc1abae
Zinc Glaze <zekegraves@gmail.com>
2018-12-10 18:41:10 -0500

    added logic for finding friend match to api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,8 @@
 //require friends array
 var allFriends = require("../data/friends");
+var smallestDifference = 1000;
+var totalDifference = 0;
+var bestMatch;
 
 module.exports = function(app) {
   //API Routes
@@ -11,9 +14,35 @@ module.exports = function(app) {
 
   //POST requests
   app.post("/api/friends", function(req, res) {
-    //responds by pushing request(form data from client) to allFriends array (server data)
-    allFriends.push(req.body);
-    res.send("New friend added!");
+
+
+    //add compatibility logic here
+
+    //req.body is the newFriend object just submitted
+    
+    //allFriends is the (not yet updated) array of allFriends
+
+    var newFriend = req.body;
+
+    for (var i = 0; i < allFriends.length; i++) {
+      
+      for (var j = 0; j < 10; j++) {
+        totalDifference += Math.abs(allFriends[i].scores[j] - newFriend.scores[j]);
+      }
+      console.log(allFriends[i].name + ": " + totalDifference);
+      if (totalDifference < smallestDifference) {
+        smallestDifference = totalDifference;
+        bestMatch = allFriends[i];
+        totalDifference = 0;
+      }
+      totalDifference = 0;
+
+
+    }
+    smallestDifference = 1000;
+    //update allFriends array (server data)by adding newFriend
+    allFriends.push(newFriend);
+    res.json(bestMatch);
   });
 };
 
